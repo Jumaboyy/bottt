251e3ca50ee588cc1d9b5355e6e50c86967dbc49
Jumaboy <jumaboynematov280@gmail.com>
2024-03-01 15:19:02 +0500

    bot.py fayli qoshdim

---
+++ dars5.py
@@ -0,0 +1,34 @@
+
+""" 5-dars  """
+
+# named tuple
+
+# from collections import namedtuple
+#
+# User=namedtuple('User','id name age')
+# users=[
+#     (1,'Jumaboy',20),
+#     (2,'Jumaboy',20),
+#     (3,'Jumaboy',20),
+#     (4,'Jumaboy',20),
+#     (5,'Jumaboy',20),
+#     (6,'Jumaboy',20),
+# ]
+#
+# for i in users:
+#     us=User(*i)
+#     print(*us)
+#
+#
+# car=('Malibu','Red','300km/h',20000,4,'Avtomat')
+# Cars=namedtuple('Cars','name color probeg price joy roles')
+# a=Cars(*car)
+# print(a.name)
+
+
+
+
+
+
+
+
